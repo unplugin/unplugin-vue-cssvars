644624f7c61f8d5253aee39c10f36e09c6001bbb
baiwusanyu-c <740132583@qq.com>
2023-03-08 17:34:11 +0800

    chore: add play

---
+++ play/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const color = ref('red')
+</script>
+
+<template>
+  <div>
+    TEST
+  </div>
+</template>
+
+<style scoped>
+ div {
+   color: v-bind(color)
+ }
+</style>
