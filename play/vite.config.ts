92288fd5afbca91c4e4f3e541b5665df52f34abc
baiwusanyu-c <740132583@qq.com>
2023-03-09 09:54:51 +0800

    chore: temp commit

--- play/vite.config.ts
+++ play/vite.config.ts
@@ -1,10 +1,27 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { viteVueCSSVars } from 'unplugin-vue-cssvars'
+import { viteVueCSSVars } from '../dist'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     minify: false,
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: 'src/main.ts',
+      name: 'index',
+      // the proper extensions will be added
+      fileName: 'index',
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['vue'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
   },
   plugins: [
     vue(),
