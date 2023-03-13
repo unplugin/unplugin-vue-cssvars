b1b50d6cd9d5060c9703b51d317d8b352ad0d938
baiwusanyu-c <740132583@qq.com>
2023-03-13 17:42:35 +0800

    chore: update ProcessCSSRes function

--- packages/core/css/process-css.ts
+++ packages/core/css/process-css.ts
@@ -5,6 +5,18 @@ import { walkCSSTree } from './pre-process-css'
 import type { ICSSFile, ICSSFileMap } from '../types'
 import type { SFCDescriptor } from '@vue/compiler-sfc'
 
+const getCSSFileRecursion = (key: string, cssFiles: ICSSFileMap, cb: (res: ICSSFile) => void) => {
+  const cssFile = cssFiles.get(key)
+  if (cssFile) {
+    cb(cssFile)
+    if (cssFile.importer.size > 0) {
+      cssFile.importer.forEach((value) => {
+        getCSSFileRecursion(value, cssFiles, cb)
+      })
+    }
+  }
+}
+
 export const createCSSModule = (descriptor: SFCDescriptor, id: string, cssFiles: ICSSFileMap) => {
   const importModule: Array<ICSSFile> = []
   for (let i = 0; i < descriptor.styles.length; i++) {
@@ -13,9 +25,11 @@ export const createCSSModule = (descriptor: SFCDescriptor, id: string, cssFiles:
 
     walkCSSTree(cssAst, content, (importer) => {
       const key = completeSuffix(path.resolve(path.parse(id).dir, importer))
-      const cssFile = cssFiles.get(key)
-      cssFile && importModule.push(cssFile)
+      getCSSFileRecursion(key, cssFiles, (res: ICSSFile) => {
+        importModule.push(res)
+      })
     }, { i: true, v: false })
   }
+  debugger
   return importModule
 }
