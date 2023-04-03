2979bb5d90b47f20dc7415401f05f491e4b96ff4
baiwusanyu-c <740132583@qq.com>
2023-04-03 17:16:12 +0800

    test: update catch error info

--- packages/core/runtime/process-css.ts
+++ packages/core/runtime/process-css.ts
@@ -29,7 +29,7 @@ export const getCSSFileRecursion = (
       })
     }
   } else {
-    throw new Error(`The writing of the path '${key}' is not standardized or undefined, which may cause \`v-bind-m\` to fail to take effect`)
+    throw new Error('path')
   }
 }
 
@@ -59,15 +59,24 @@ export const getVBindVariableListByPath = (
     parseImporterRes.imports.forEach((res) => {
       const importerPath = resolve(idDirParse.dir, res.path)
 
-      // 根据 @import 信息，从 cssFiles 中，递归的获取所有在预处理时生成的 cssvars 样式
-      getCSSFileRecursion(lang, importerPath, cssFiles, (res: ICSSFile) => {
-        if (res.vBindCode) {
-          !server && injectCSSContent.add({ content: res.content, lang: res.lang, styleTagIndex: i })
-          res.vBindCode.forEach((vb) => {
-            vbindVariable.add(vb)
-          })
+      try {
+        // 根据 @import 信息，从 cssFiles 中，递归的获取所有在预处理时生成的 cssvars 样式
+        getCSSFileRecursion(lang, importerPath, cssFiles, (res: ICSSFile) => {
+          if (res.vBindCode) {
+            !server && injectCSSContent.add({ content: res.content, lang: res.lang, styleTagIndex: i })
+            res.vBindCode.forEach((vb) => {
+              vbindVariable.add(vb)
+            })
+          }
+        })
+      } catch (e) {
+        if ((e as Error).message === 'path') {
+          const doc = 'https://github.com/baiwusanyu-c/unplugin-vue-cssvars/pull/29'
+          throw new Error(`Unable to resolve file under path '${res.path}', see: ${doc}`)
+        } else {
+          throw new Error((e as Error).message)
         }
-      })
+      }
     })
   }
   return {
